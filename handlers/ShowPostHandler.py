533cf27d9d61b0dbddd286b770a4c3334433a05a
Franco Sarachu <fsarachu@gmail.com>
2016-11-09 16:42:46 -0300

    Return 404 if post does not exists

--- handlers/ShowPostHandler.py
+++ handlers/ShowPostHandler.py
@@ -5,4 +5,9 @@ from entities import *
 class ShowPostHandler(Handler):
     def get(self, post_id):
         post = Post.get_by_id(int(post_id))
+
+        if not post:
+            self.error(404)
+            return
+
         self.render('post.html', post=post)
