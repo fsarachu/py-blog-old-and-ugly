371feb37e7eb3cd95c5584e4decba62379964fd3
Franco Sarachu <fsarachu@gmail.com>
2016-11-09 00:10:26 -0300

    Print posts on mainpage

--- handlers/MainHandler.py
+++ handlers/MainHandler.py
@@ -1,6 +1,9 @@
+from google.appengine.ext import db
+
 from Handler import Handler
 
 
 class MainHandler(Handler):
     def get(self):
-        self.render('index.html')
+        posts = db.GqlQuery('SELECT * FROM Post ORDER BY created DESC')
+        self.render('index.html', posts=posts)
