535cce744fcccea7c29f9674226b61686976f6f6
Franco Sarachu <fsarachu@gmail.com>
2016-11-09 00:02:32 -0300

    Define post method on NewPostHandler.py

--- handlers/NewPostHandler.py
+++ handlers/NewPostHandler.py
@@ -1,6 +1,19 @@
 from Handler import Handler
+from entities import *
 
 
 class NewPostHandler(Handler):
     def get(self):
         self.render('new_post.html')
+
+    def post(self):
+        title = self.request.get('title')
+        content = self.request.get('content')
+
+        if title and content:
+            new_post = Post(title=title, content=content)
+            new_post.put()
+            self.redirect('/')
+        else:
+            error = 'You must provide both title and content'
+            self.render('new_post.html', title=title, content=content)
